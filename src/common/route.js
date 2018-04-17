7cd1810bfc572b5639b65eaa7934f3e73a67ec99
rbhatnagar <rbhatnagar@activehealth.net>
2018-04-17 16:21:35 -0400

    Updating the route file

--- src/common/route.js
+++ src/common/route.js
@@ -4,20 +4,20 @@
 
 "use strict";
 
-import React from 'react';
-import '../css/App.css';
 import News from '../js/component/News';
-import HOA from '../js/component/HOA';
+import Member from '../js/component/Member';
 import Home from '../js/component/Home';
+import Home1 from '../js/component/Home1';
 import About from '../js/component/About';
+import ActiveHealthHome from '../js/component/ActiveHealthHome';
 import App from '../js/App.jsx';
 
 const routes = [
-    { path: '/', component:Home },
+    { path: '/', exact:true, component:Home },
     { path: '/about', component:About },
-    { path: '/hoa',  component:HOA },
-    { path: '/home',  component:Home },
-    { path: '/spHome',  component:App }
+    { path: '/member',  component:Member },
+    { path: '/news',  component:News },
+    { path: '/spHome',  component:ActiveHealthHome }
 ];
 
 export default routes;

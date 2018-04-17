93a8ccaa7217d449824c8f114a05218ce6193b26
rbhatnagar <rbhatnagar@activehealth.net>
2018-04-17 16:19:06 -0400

    Updating

--- src/js/component/News.js
+++ src/js/component/News.js
@@ -1,13 +1,27 @@
 'use strict';
 
 import React from 'react';
+import About from './About';
 
 export default class News extends React.Component{
 
     render(){
         return(
             <div>
-                <h2>NEWS PAGE...</h2>
+                <div className="main-wrapper">
+                    <div className="container">
+                        <div className="row">
+                            <div className="fluid-space">
+                                <div className="row">
+                                    <div className="span12">
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <About />
+                </div>
             </div>
         )
     }
